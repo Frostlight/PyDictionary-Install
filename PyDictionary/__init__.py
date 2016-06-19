83551375e7ce8481b349a41ab32143eca2aa5676
Vincent <cloudy.sonata@gmail.com>
2016-06-19 15:33:09 -0400

    Allow multi term

--- PyDictionary/__init__.py
+++ PyDictionary/__init__.py
@@ -52,41 +52,33 @@ class PyDictionary(object):
         return [self.translate(term, language) for term in self.args]
 
     def translate(self, term, language):
-        if len(term.split()) > 1:
+        try:
+            gs = goslate.Goslate()
+            word = gs.translate(term, language)
+            return word
+        except:
             return None
-            #print("Error: A Term must be only a single word")
-        else:
-            try:
-                gs = goslate.Goslate()
-                word = gs.translate(term, language)
-                return word
-            except:
-                return None
-                #print("Invalid Word")
+            #print("Invalid Word")
 
     def getSynonyms(self, formatted=True):
         return [self.synonym(term, formatted) for term in self.args]
 
     @staticmethod
     def synonym(term, formatted=False):
-        if len(term.split()) > 1:
+        try:
+            data = _get_soup_object("http://www.thesaurus.com/browse/{0}".format(term))
+            terms = data.select("div#filters-0")[0].findAll("li")
+            if len(terms) > 5:
+                terms = terms[:5:]
+            li = []
+            for t in terms:
+                li.append(t.select("span.text")[0].getText())
+            if formatted:
+                return {term: li}
+            return li
+        except:
             return None
-            #print("Error: A Term must be only a single word")
-        else:
-            try:
-                data = _get_soup_object("http://www.thesaurus.com/browse/{0}".format(term))
-                terms = data.select("div#filters-0")[0].findAll("li")
-                if len(terms) > 5:
-                    terms = terms[:5:]
-                li = []
-                for t in terms:
-                    li.append(t.select("span.text")[0].getText())
-                if formatted:
-                    return {term: li}
-                return li
-            except:
-                return None
-                #print("{0} has no Synonyms in the API".format(term))
+            #print("{0} has no Synonyms in the API".format(term))
 
     def __repr__(self):
         return "<PyDictionary Object with {0} words>".format(len(self.args))
@@ -102,76 +94,64 @@ class PyDictionary(object):
 
     @staticmethod
     def antonym(word, formatted=False):
-        if len(word.split()) > 1:
+        try:
+            data = _get_soup_object("http://www.thesaurus.com/browse/{0}".format(word))
+            terms = data.select("section.antonyms")[0].findAll("li")
+            if len(terms) > 5:
+                terms = terms[:5:]
+            li = []
+            for t in terms:
+                li.append(t.select("span.text")[0].getText())
+            if formatted:
+                return {word: li}
+            return li
+        except:
             return None
-            #print("Error: A Term must be only a single word")
-        else:
-            try:
-                data = _get_soup_object("http://www.thesaurus.com/browse/{0}".format(word))
-                terms = data.select("section.antonyms")[0].findAll("li")
-                if len(terms) > 5:
-                    terms = terms[:5:]
-                li = []
-                for t in terms:
-                    li.append(t.select("span.text")[0].getText())
-                if formatted:
-                    return {word: li}
-                return li
-            except:
-                return None
-                #print("{0} has no Antonyms in the API".format(word))
+            #print("{0} has no Antonyms in the API".format(word))
 
     @staticmethod
     def meaning(term, disable_errors=False):
-        if len(term.split()) > 1:
-            return None
-            #print("Error: A Term must be only a single word")
-        else:
-            try:
-                html = _get_soup_object("http://wordnetweb.princeton.edu/perl/webwn?s={0}".format(
-                    term))
-                types = html.findAll("h3")
-                length = len(types)
-                lists = html.findAll("ul")
-                out = {}
-                for a in types:
-                    reg = str(lists[types.index(a)])
-                    meanings = []
-                    for x in re.findall(r'\((.*?)\)', reg):
-                        if 'often followed by' in x:
-                            pass
-                        elif len(x) > 5 or ' ' in str(x):
-                            meanings.append(x)
-                    name = a.text
-                    out[name] = meanings
-                return out
-            except Exception as e:
-                if disable_errors == False:
-                    return None
-                    #print("Error: The Following Error occured: %s" % e)
+        try:
+            html = _get_soup_object("http://wordnetweb.princeton.edu/perl/webwn?s={0}".format(
+                term))
+            types = html.findAll("h3")
+            length = len(types)
+            lists = html.findAll("ul")
+            out = {}
+            for a in types:
+                reg = str(lists[types.index(a)])
+                meanings = []
+                for x in re.findall(r'\((.*?)\)', reg):
+                    if 'often followed by' in x:
+                        pass
+                    elif len(x) > 5 or ' ' in str(x):
+                        meanings.append(x)
+                name = a.text
+                out[name] = meanings
+            return out
+        except Exception as e:
+            if disable_errors == False:
+                return None
+                #print("Error: The Following Error occured: %s" % e)
 
     @staticmethod
     def googlemeaning(term, formatted=True):
-        if len(term.split()) > 1:
+        try:
+            html = _get_soup_object("http://www.google.co.in/search?q=define:%3A%20{0}".format(
+                term))
+            body = html.find(
+                "table", {"style": "font-size:14px;width:100%"})
+            wordType = body.find(
+                "div", {"style": "color:#666;padding:5px 0"}).getText()
+            meaning = body.find("li").getText()
+            formated = "{0} : {1} \n{2}\n".format(
+                term.capitalize(), wordType, meaning)
+            if not formatted:
+                return meaning
+            return formated
+        except Exception as e:
             return None
-            #print("Error: A Term must be only a single word")
-        else:
-            try:
-                html = _get_soup_object("http://www.google.co.in/search?q=define:%3A%20{0}".format(
-                    term))
-                body = html.find(
-                    "table", {"style": "font-size:14px;width:100%"})
-                wordType = body.find(
-                    "div", {"style": "color:#666;padding:5px 0"}).getText()
-                meaning = body.find("li").getText()
-                formated = "{0} : {1} \n{2}\n".format(
-                    term.capitalize(), wordType, meaning)
-                if not formatted:
-                    return meaning
-                return formated
-            except Exception as e:
-                return None
-                #print("Error: The Word given is not a valid English Word")
+            #print("Error: The Word given is not a valid English Word")
 
 if __name__ == '__main__':
     d = PyDictionary('honest','happy')
